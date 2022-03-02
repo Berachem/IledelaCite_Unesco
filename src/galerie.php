96931933696cd4cf9532fe0002c49aa642346e64
Berachem Markria <berachem.markria@gmail.com>
2022-03-02 17:28:09 +0100

    base terminée pour la gallerie

--- src/galerie.php
+++ src/galerie.php
@@ -12,33 +12,93 @@ echo "
       <h1>".$nav_gallery[$langue]."</h1>
     </div>
   ";
+  ?>
 
-  echo '
-  <div class="lightbox-gallery">
-    <div class="container">
-        <div class="row photos">
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><img class="img-fluid" src="../img/saintlouis.jpeg"></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><img class="img-fluid" src="https://i.imgur.com/gX11Vt5.jpg"></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><img class="img-fluid" src="https://i.imgur.com/pZcUS2Y.jpg"></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><img class="img-fluid" src="https://i.imgur.com/06Ajq7f.jpg"></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><img class="img-fluid" src="https://i.imgur.com/zmzERpe.jpg"></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><img class="img-fluid" src="https://i.imgur.com/gX11Vt5.jpg"></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><img class="img-fluid" src="https://i.imgur.com/pZcUS2Y.jpg"></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><img class="img-fluid" src="https://i.imgur.com/06Ajq7f.jpg"></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><img class="img-fluid" src="https://i.imgur.com/zmzERpe.jpg"></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><img class="img-fluid" src="https://i.imgur.com/gX11Vt5.jpg"></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><img class="img-fluid" src="https://i.imgur.com/pZcUS2Y.jpg"></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><img class="img-fluid" src="https://i.imgur.com/06Ajq7f.jpg"></a></div>
-            <div class="col-sm-6 col-md-4 col-lg-3 item"><img class="img-fluid" src="https://i.imgur.com/zmzERpe.jpg"></a></div>
-        </div>
+<!-- Phrase accroche -->
+<div class="description">
+        <p>
+          L'Île de la Cité a toujours été une grande inspiration pour les artistes de toutes les époques. <i class="fas fa-paint-brush"></i>
+        </p>
     </div>
+    <br>
+
+  <div class="container">
+        <div class="row d-flex flex-wrap align-items-center" data-toggle="modal" data-target="#lightbox">
+          <div class="col-12 col-md-6 col-lg-3">
+            
+        <img src="../img/galerie/1.jpg" data-target="#indicators" data-slide-to="0" alt="" data-toggle="tooltip" data-placement="top" title="The Quai Saint-Michel and Notre-Dame, Maximilien Luce, 1901"/> 
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+              <img src="../img/galerie/2.jpg" data-target="#indicators" data-slide-to="1" alt="" data-toggle="tooltip" data-placement="top" title="Frank Boggs, Quai à la Seinie, Paris, au Clair de Lune, 1898"/>
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+            <img src="../img/galerie/3.jpeg" data-target="#indicators" data-slide-to="2"  alt="" />
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+              <img src="../img/galerie/4.jpg" data-target="#indicators" data-slide-to="3" alt="" />
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+              <img src="../img/galerie/5.jpg" data-target="#indicators" data-slide-to="3"  alt="" />
+          </div>
+          <div class="col-12 col-md-6 col-lg-3">
+              <img src="../img/galerie/6.jpg" data-target="#indicators" data-slide-to="4" alt="" />
+          </div>
+        </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="lightbox" role="dialog" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <button type="button" class="close text-right p-2" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              <div id="indicators" class="carousel slide" data-interval="false">
+          <ol class="carousel-indicators">
+            <li data-target="#indicators" data-slide-to="0" class="active"></li>
+            <li data-target="#indicators" data-slide-to="1"></li>
+            <li data-target="#indicators" data-slide-to="2"></li>
+            <li data-target="#indicators" data-slide-to="3"></li>
+            <li data-target="#indicators" data-slide-to="4"></li>
+            <li data-target="#indicators" data-slide-to="5"></li>
+          </ol>
+          <div class="carousel-inner">
+            
+            <div class="carousel-item active">
+              
+              <img class="d-block w-100" src="../img/galerie/1.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="../img/galerie/2.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="../img/galerie/3.jpeg" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="../img/galerie/4.jpg" alt="Fourth slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="../img/galerie/5.jpg" alt="Fifth slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="../img/galerie/6.jpg" alt="Sixth slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#indicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#indicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+
+            </div>
+          </div>
+        </div>
 </div>
-  ';
 
 
-
-
-echo "
     <footer>
       <img src='../img/logo.png' alt='Logo Unescite' id='logoIle' >
 
@@ -60,6 +120,5 @@ echo "
         </div>
     </footer>
   </body>
-</html>";
+</html>"
 
-?>
