849b4b80dd385f0099376cdef584812cb8bab758
Berachem Markria <61350744+Berachem@users.noreply.github.com>
2022-04-06 10:03:32 +0200

    form admin

--- src/admin.php
+++ src/admin.php
@@ -16,26 +16,30 @@ include('../php/header.inc.php');
 
 
     <p class="presentation">
-      Vous pourrez <b style='color: #f7af3e;'> modifier des éléments </b> et <b style='color: #f7af3e;'> gérer la base de données</b>. <img src="../img/bdd.png"  width="45" height="45" alt="BDD">
-    <br>
+        Vous pourrez <b style='color: #f7af3e;'> modifier des éléments </b> et <b style='color: #f7af3e;'> gérer la base de données</b>. <img src="../img/bdd.png"  width="45" height="45" alt="BDD">
       
-    <br>
-    <form>
-        <div class="form-group" action="adminlogin.php" method="POST>
-          <label for="exampleInputEmail1">Identifiant</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entrez votre identifiant">
-          <small id="emailHelp" class="form-text text-muted">Entrez les 6 chiffres...</small>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Mot de passe</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
-        <div class="form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Rester connecté</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Connexion</button>
-    </form>
+        <br>
+        <br>
+
+        <form>
+          <div class="form-group" action="adminlogin.php" method="POST>
+            <label for="exampleInputEmail1">Identifiant</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entrez votre identifiant">
+            <small id="emailHelp" class="form-text text-muted">Entrez les 6 chiffres...</small>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Mot de passe</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+          </div>
+          <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Rester connecté</label>
+          </div>
+          <button type="submit" class="btn btn-primary">Connexion</button>
+      </form>
+
+    </p>
+
 
 
     <?php
