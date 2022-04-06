439f0f282d1cc8f1e7a7de18b23bd40d7fdfd446
Berachem Markria <61350744+Berachem@users.noreply.github.com>
2022-04-06 10:01:42 +0200

    page admin

--- src/admin.php
+++ src/admin.php
@@ -16,15 +16,15 @@ include('../php/header.inc.php');
 
 
     <p class="presentation">
-      Vous pourrez <b style='color: #f7af3e;'> modifier des éléments </b> et gérer la base de données.
+      Vous pourrez <b style='color: #f7af3e;'> modifier des éléments </b> et <b style='color: #f7af3e;'> gérer la base de données</b>. <img src="../img/bdd.png"  width="45" height="45" alt="BDD">
     <br>
       
     <br>
     <form>
-        <div class="form-group">
+        <div class="form-group" action="adminlogin.php" method="POST>
           <label for="exampleInputEmail1">Identifiant</label>
           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entrez votre identifiant">
-          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          <small id="emailHelp" class="form-text text-muted">Entrez les 6 chiffres...</small>
         </div>
         <div class="form-group">
           <label for="exampleInputPassword1">Mot de passe</label>
--- src/parcours.php
+++ src/parcours.php
@@ -16,7 +16,7 @@ include('../php/header.inc.php');
 
 
     <p class="presentation">
-      Vous pourrez réaliser ce parcours peut vous prendre <b style='color: #f7af3e;'> 2 heures </b> ou bien <b style='color: #f7af3e;'>une journée entière</b> si vous êtes curieux et attentionné. <img src="../img/question.png"  width="45" height="45" alt="merci">
+      Vous pourrez réaliser ce parcours peut vous prendre <b style='color: #f7af3e;'> 2 heures </b> ou bien <b style='color: #f7af3e;'>une journée entière</b> si vous êtes curieux et attentionné. <img src="../img/question.png"  width="45" height="45" alt="parcours">
     <br>
       
     <iframe id= "Rallye_map" width="100%" height="600px" frameborder="0" allowfullscreen src="https://www.google.com/maps/d/u/0/embed?mid=1vSrjAa3AV4QliLCg__vC7r_eriF9yZ2I&ehbc=2E312F"></iframe>
