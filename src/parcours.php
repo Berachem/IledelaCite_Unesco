029bf2b90e89899d210dd3df302fcd2992f50dac
Berachem Markria <61350744+Berachem@users.noreply.github.com>
2022-04-20 10:16:20 +0200

    2 parcours

--- src/parcours.php
+++ src/parcours.php
@@ -18,13 +18,30 @@ include('../php/header.inc.php');
     </div>
     <br>
 
-    <iframe id= "Rallye_map" width="100%" height="600px" frameborder="0" allowfullscreen src="https://www.google.com/maps/d/u/0/embed?mid=1vSrjAa3AV4QliLCg__vC7r_eriF9yZ2I&ehbc=2E312F"></iframe>
+    <p>
+      <a class="btn btn-primary" data-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapse1">
+        PArcours Promenade
+      </a>
+      <a class="btn btn-primary" data-toggle="collapse" href="#collapse2" role="button" aria-expanded="false" aria-controls="collapse2">
+        Parcours Marathon
+      </a>
+  </p>
+  <div class="collapse" id="collapse1">
+    <div class="card card-body">
+      <iframe id= "Rallye_map" width="100%" height="600px" frameborder="0" allowfullscreen src="https://www.google.com/maps/d/u/0/embed?mid=1vSrjAa3AV4QliLCg__vC7r_eriF9yZ2I&ehbc=2E312F"></iframe>
+    </div>
+  </div>
+  <div class="collapse" id="collapse2">
+    <div class="card card-body">
+     <iframe id= "Rallye_map" width="100%" height="600px" frameborder="0" allowfullscreen src="https://www.google.com/maps/d/u/0/edit?mid=1SgylBzEgvyETxx28E5v391zrw_n1Hsj7&usp=sharing"></iframe>
+    </div>
+  </div>
 
-      <br>
-      <br>
+
+    <br>
+
       
 
-      <iframe id= "Rallye_map" width="100%" height="600px" frameborder="0" allowfullscreen src="https://www.google.com/maps/d/u/0/edit?mid=1SgylBzEgvyETxx28E5v391zrw_n1Hsj7&usp=sharing"></iframe>
 
 
     <br>
