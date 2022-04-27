596923b4ea515d423dc2b68df12fb98bb2f9a642
Berachem Markria <berachem.markria@gmail.com>
2022-04-27 22:48:32 +0200

    fix liens footer

--- php/footer.inc.php
+++ php/footer.inc.php
@@ -34,7 +34,7 @@
 <div id='LogosFooter'></div>
     <div class='footer-social-icons'>
         <ul class='social-icons'>
-            <li><a href='https://www.facebook.com/profile.php?id=100075301764425' class='social-icon'> <i class='fa fa-facebook'></i></a></li>
+            <li><a href='https://www.facebook.com/Ile-de-la-Cit%C3%A9-Unesco-107475771919467' class='social-icon'> <i class='fa fa-facebook'></i></a></li>
             <li><a href='https://twitter.com/IleCiteParis' class='social-icon'> <i class='fa fa-twitter'></i></a></li>
             <li><a href='' class='social-icon'> <i class='fa fa-instagram'></i></a></li>
             <li><a href='https://fr.unesco.org/' class='social-icon'> <i class='fas fa-landmark'></i></a></li>
@@ -42,10 +42,10 @@
 
             <?php
               if ($page=="A propos"){
-                echo "<li><a href='https://github.com/Berachem' class='social-icon'> <i class='fa fa-github' style='color:white;'></i></a></li>";
+                echo "<li><a href='https://github.com/Berachem/IledelaCite_Unesco' class='social-icon'> <i class='fa fa-github' style='color:white;'></i></a></li>";
                 echo "<li><a target='_self' href='admin_connect.php' class='social-icon'> <i class='fa fa-wrench'></i></a></li>";
               }else{
-                echo "<li><a href='https://github.com/Berachem' class='social-icon'> <i class='fa fa-github'></i></a></li>";
+                echo "<li><a href='https://github.com/Berachem/IledelaCite_Unesco' class='social-icon'> <i class='fa fa-github'></i></a></li>";
               }
 
               ?>
