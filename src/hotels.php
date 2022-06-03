8e87b2221ed57ea637f849d22bffc479ec4e9a86
Berachem Markria <berachem.markria@gmail.com>
2022-06-03 20:22:28 +0200

    MANQUE : changer liens images hotels

--- src/hotels.php
+++ src/hotels.php
@@ -23,29 +23,35 @@ while ($line = fgets($txt_file)) {
 print_r($data);
 
  echo '
-      <div class="card bg-dark text-white">
-        <img class="card-img" src='.$data[2].' alt="Card image">
-        <div class="card-img-overlay">
+      <div class="card bg-dark text-white hotel-card" >
+        <img class="card-img-top" src='.$data[2].' alt="Card image">
+        <div class="card-img-body">
           <h5 class="card-title">'.str_replace('"', '', $data[0]).'</h5>
           <p class="card-text"> ';
           echo "<i class='fas fa-map-marker-alt' style='color: #f7af3e;'></i> ";
           echo str_replace('"', '', $data[3]);
           echo "<br>";
-          echo "<i class='fas fa-euro-sign' style='color: #f7af3e;'></i> ";
+         
           $prix = str_replace('€ ', '', $data[6]);
+          
           $prix = str_replace('"', '', $prix);
           echo $prix;
+          echo "<i class='fas fa-euro-sign' style='color: #f7af3e;'></i> ";
           echo"  /  <i class='fas fa-moon' style='color: #f7af3e;'></i> ";
           echo "<br>";
           echo "<i class='fas fa-star' style='color: #f7af3e;'></i> ";
           echo str_replace('"', '', $data[5]);
-          echo "<br>";
+          
+          
           echo '</p>;
         </div>
       </div>';
  echo '
  </section>
- 
+ <br>
+ <br>
+ <br>
+ <br>
  <br>';
 
  $a++;
