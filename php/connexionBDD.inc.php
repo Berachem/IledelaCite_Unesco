69193ffeba3196896464b7c5a31f77366328af75
Berachem Markria <61350744+Berachem@users.noreply.github.com>
2022-06-17 17:07:36 +0200

    Update connexionBDD.inc.php

--- php/connexionBDD.inc.php
+++ php/connexionBDD.inc.php
@@ -3,8 +3,8 @@
 /*
  * création d'objet PDO de la connexion qui sera représenté par la variable $cnx
  */
-$user =  'berachem.markria';
-$pass =  'Bouzened93';
+$user =  'SECRET';
+$pass =  'SECRET';
 
 
 
